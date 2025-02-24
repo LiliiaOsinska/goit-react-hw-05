8875c86c668171b96b794ded498592198282ab24
LiliiaOsinska <lo.lihcka@gmail.com>
2025-02-24 16:21:10 +0100

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import "modern-normalize";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <>
-        <App />
-        <Toaster position="top-right" />
-        пше
-      </>
-    </BrowserRouter>
+    <>
+      <App />
+      <Toaster position="top-right" />
+    </>
   </StrictMode>
 );
